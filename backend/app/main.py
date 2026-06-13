f39b40f9bfb893722ea1ac66b7426343bda24975
Beelka <162022437+beelkaa@users.noreply.github.com>
2026-06-13 03:05:19 +0100

    Set allow_credentials to False in CORS middleware
    Changed CORS middleware to disallow credentials.

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,10 +20,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
 )
 
 app.include_router(auth.router)
