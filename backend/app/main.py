43fc53c6f19a1b809f1f8d5ad15e158141327066
Beelka <162022437+beelkaa@users.noreply.github.com>
2026-06-13 02:45:39 +0100

    Update CORS middleware to allow all origins
    Changed CORS middleware to allow all origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,10 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://recrutia-eight.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
